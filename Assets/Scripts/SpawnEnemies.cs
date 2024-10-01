f5dfbc4e8113ca4371247987c060490e55bc0502
dita-hss <101124244+dita-hss@users.noreply.github.com>
2024-10-01 10:23:36 -0600

    10/01 updates

--- Assets/Scripts/SpawnEnemies.cs
+++ Assets/Scripts/SpawnEnemies.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// currently debugging on why no new black or red among us enemies are spawning. 
+// it shows that ht e count on the list is always 7 so it never meets the condition to spawn new ones
+// but it is reaching the "destroy" function so im not sure what is going on 
+// i think i need a "CubeHit" script seperate for the black and red among us enemies
 public class SpawnEnemies : MonoBehaviour {
     public GameObject CubeTemplate;
     public GameObject AmongUsTemplate;
     public GameObject blackAmongUsTemplate;
 
-    public int maxEnemies = 21;
+    public int maxEnemiesCubes = 7;
+    public int maxEnemiesRed = 7;
+    public int maxEnemiesBlack = 7;
 
     // keeping track of the enemies that are spawned and where they can be spawned
     private List<GameObject> cubeEnemies = new List<GameObject>();
@@ -19,23 +25,24 @@ public class SpawnEnemies : MonoBehaviour {
 
     void Start() {
         planeRenderer = plane.GetComponent<Renderer>();
-        for (int i = 0; i < maxEnemies; i++) {
+        for (int i = 0; i < maxEnemiesCubes; i++) {
             SpawnCubeEnemy();
             SpawnAmongUsEnemy();
+            SpawnBlackAmongUsEnemy();
         }
     }
 
     void Update() {
         // make sure there is always x amount of enemies
-        if (cubeEnemies.Count < maxEnemies / 3) {
+        if (cubeEnemies.Count < maxEnemiesCubes) {
             SpawnCubeEnemy();
         }
 
-        if (amongUsEnemies.Count < maxEnemies / 3) {
+        if (amongUsEnemies.Count < maxEnemiesRed) {
             SpawnAmongUsEnemy();
         }
 
-        if (blackAmongUsEnemies.Count < maxEnemies / 3) {
+        if (blackAmongUsEnemies.Count < maxEnemiesBlack) {
             SpawnBlackAmongUsEnemy();
         }
     }
@@ -43,20 +50,22 @@ public class SpawnEnemies : MonoBehaviour {
     void SpawnCubeEnemy() {
         // some random position on the surface of the plane , spawn an enemy , add to tracker
             //Debug.Log("Spawning cube enemy");
+            // Debug.Log(amongUsEnemies.Count);
+            // Debug.Log(blackAmongUsEnemies.Count);
         Vector3 randomPosition = GetRandomPositionOnPlane();
         GameObject newEnemy = Instantiate(CubeTemplate, randomPosition, transform.rotation);
         cubeEnemies.Add(newEnemy);
     }
 
     void SpawnAmongUsEnemy() {
-            //Debug.Log("Spawning red among us enemy");
+            Debug.Log("Spawning red among us enemy");
         Vector3 randomPosition = GetRandomPositionOnPlane();
         GameObject newAmongUsEnemy = Instantiate(AmongUsTemplate, randomPosition, transform.rotation);
         amongUsEnemies.Add(newAmongUsEnemy);
     }
 
     void SpawnBlackAmongUsEnemy() {
-            //Debug.Log("Spawning black among us enemy");
+            Debug.Log("Spawning black among us enemy");
         Vector3 randomPosition = GetRandomPositionOnPlane();
         GameObject newBlackAmongUsEnemy = Instantiate(blackAmongUsTemplate, randomPosition, transform.rotation);
         blackAmongUsEnemies.Add(newBlackAmongUsEnemy);
@@ -72,11 +81,14 @@ public class SpawnEnemies : MonoBehaviour {
     public void DestroyAmongUsEnemy(GameObject amongUsEnemy){
         amongUsEnemies.Remove(amongUsEnemy);
         Destroy(amongUsEnemy);
+            Debug.Log(amongUsEnemies.Count);
+            
     }
 
     public void DestroyBlackAmongUsEnemy(GameObject blackAmongUsEnemy){
         blackAmongUsEnemies.Remove(blackAmongUsEnemy);
         Destroy(blackAmongUsEnemy);
+            Debug.Log(blackAmongUsEnemies.Count);
     }
 
     Vector3 GetRandomPositionOnPlane() {
@@ -88,7 +100,7 @@ public class SpawnEnemies : MonoBehaviour {
         float randomZ = Random.Range(planeBounds.min.z, planeBounds.max.z);
 
         // so its not inside
-        float yPosition = planeBounds.max.y + 0.5f;
+        float yPosition = planeBounds.max.y + 0.1f;
 
         return new Vector3(randomX, yPosition, randomZ);
     }
